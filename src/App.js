a3c474a9df0aa55d26ce8c6935a31d7dd7659b77
Thomas Maier <mjcross@hotmail.de>
2018-12-23 13:40:49 +0100

    Improved Sidemenu

--- src/App.js
+++ src/App.js
@@ -1,44 +1,65 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.scss';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.scss";
 
 //import my own modules
-import Imprint from './components/Imprint';
-import FAQ from './components/faq';
+import Imprint from "./components/Imprint";
+import FAQ from "./components/faq";
 
 const contentful = require("contentful");
 
 const client = contentful.createClient({
   space: "pntshaoi0gaf",
-  accessToken: "0c5358d8334cd3f6fc85ff1eae5cb6c4689d1b1f9db29177d783286480d7fd46"
+  accessToken:
+    "0c5358d8334cd3f6fc85ff1eae5cb6c4689d1b1f9db29177d783286480d7fd46"
 });
 
-class App extends Component { 
+class App extends Component {
   render() {
     return (
       <Router className="App">
-        <div>
-          <section>
-            <div className="columns is-fullheight">
-              <div className="column is-1 is-sidebar-menu is-hidden-mobile">
-                <aside className="menu">
-                  <p className="menu-label">
-                    General
-                  </p>
-                  <ul className="menu-list">
-                    <li>Dashboard</li>
-                    <li>Help</li>
-                  </ul>
-                </aside>
+        <section className="columns is-gapless">
+          <div className="column is-1 has-background-light">
+            <aside className="columns is-centered is-multiline">
+              <div className="column is-12">
+                <br />
               </div>
-              <div className="column">
-                <Route exact path="/impressum/de" render={(props) => ( <Imprint contentful={client} locale="de-DE"/> )} />
-                <Route path="/impressum/en" render={(props) => ( <Imprint contentful={client} locale="en-GB"/> )} />
-                <Route path="/faq/" render={(props) => (<FAQ contentful={client}/>)} />
+              <div className="column is-narrow">
+                <figure class="image is-128x128">
+                  <img
+                    src="https://images.ctfassets.net/pntshaoi0gaf/5n8rKxJhv2CQw26iCyysi6/acf3944e39cca76a5d212001c58adfb9/VW-Logo.png"
+                    alt="VW Logo"
+                  />
+                </figure>
               </div>
-            </div>
-          </section>
-        </div>
+              <div className="column is-12"></div>
+              <div className="column is-narrow">
+                <Link to="/" className="title is-size-5 has-text-black">CAR-NET</Link>
+              </div>
+              <div className="column is-12">
+                <br />
+              </div>
+              <div className="column is-narrow">
+                <Link to="/impressum/de" className="title is-size-5 has-text-black">
+                  Impressum
+                </Link>
+              </div>
+            </aside>
+          </div>
+
+          <div className="column is-11 is-fullheight">
+            <Route
+              exact
+              path="/impressum/de"
+              render={props => <Imprint contentful={client} locale="de-DE" />}
+            />
+            <Route
+              path="/impressum/en"
+              render={props => <Imprint contentful={client} locale="en-GB" />}
+            />
+            <Route path="/faq/" render={props => <FAQ contentful={client} />} />
+          </div>
+        </section>
       </Router>
     );
   }
--- src/components/Imprint.js
+++ src/components/Imprint.js
@@ -1,67 +1,75 @@
-import React, { Component } from 'react';
-import Section from './contentBlock';
-import RichText from './richtext';
+import React, { Component } from "react";
+import Section from "./contentBlock";
+import RichText from "./richtext";
 
 class Imprint extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            entry: ""
-        };
-    }
+    this.state = {
+      entry: ""
+    };
+  }
 
-    componentDidMount() {
-        this.props.contentful.getEntries({
-            'content_type': "impressum",
-            'locale': this.props.locale
-        }).then( (entries) => {
-
-            this.setState({
-                entry: entries.items[0].fields
-            });
-            console.log(this.state.entry);
+  componentDidMount() {
+    this.props.contentful
+      .getEntries({
+        content_type: "impressum",
+        locale: this.props.locale
+      })
+      .then(entries => {
+        this.setState({
+          entry: entries.items[0].fields
         });
+        console.log(this.state.entry);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  renderSections() {
+    const sections = this.state.entry.sections.map(section => (
+      <p>
+        <Section
+          header={section.fields["header"]}
+          content={section.fields["content"]}
+        />
+        <br />
+      </p>
+    ));
+
+    return <div className="column is-8">{sections}</div>;
+  }
+
+  render() {
+    var sections = "";
+
+    if (this.state.entry !== "") {
+      sections = this.renderSections();
     }
 
-    renderSections() {
-        const sections = this.state.entry.sections.map((section) => ( <p><Section header={section.fields['header']} content={section.fields['content']}/><br /></p> ));
-
-        return(
-            <div className="column is-8">
-                {sections}
+    return (
+      <div>
+        <section className="hero is-info">
+          <div className="hero-body content">
+            <h1 className="title has-text-centered">
+              {this.state.entry.title}
+            </h1>
+          </div>
+        </section>
+        <section className="hero">
+          <div className="hero-body columns is-centered is-multiline">
+            <div className="column is-8 content">
+              <h2>{this.state.entry.header}</h2>
+              <RichText richtext={this.state.entry.intro} />
             </div>
-        )
-    }
-
-    render() {
-        var sections = "";
-
-        if (this.state.entry !== ""){
-            sections = this.renderSections();
-        }
-        
-        return (
-            <div>
-                <section className="hero is-info">
-                    <div className="hero-body">
-                        <h1 className="title has-text-centered">
-                            { this.state.entry.title }
-                        </h1>
-                    </div>
-                </section>
-                <section className="hero">
-                    <div className="hero-body columns is-centered is-multiline">
-                        <div className="column is-8 content">
-                            <h2>{ this.state.entry.header }</h2>
-                            <RichText richtext={this.state.entry.intro}/>
-                        </div>
-                        {sections}
-                    </div>
-                </section>
-            </div>
-        );
-    }
+            {sections}
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Imprint;
