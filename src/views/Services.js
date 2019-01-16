b5a7ae57c8fad87e01783b8a6479c83866c7eabd
My Trang Hong <mytrang.hong@kreuzwerker.de>
2019-01-16 13:29:37 +0100

    change service hmi view

--- src/views/Services.js
+++ src/views/Services.js
@@ -14,17 +14,20 @@ class Services extends Component {
         activeIndex: 0,
         modus: "desktop",
         desktop: "",
-        hmi: ""
+        hmi: "",
+        hmiService: ""
       };
 
     componentDidMount() {
         this.fetchData();
+        this.fetchServiceById('nymVB60QpwseC0eEkyEck');
         this.setState({ activeIndex: FINs.indexOf(this.props.match.params.id)})
 
       }
 
     componentWillReceiveProps(newProps) {
     this.props = newProps;
+    this.fetchServiceById('nymVB60QpwseC0eEkyEck');
     this.fetchData();
     }
 
@@ -59,92 +62,42 @@ class Services extends Component {
                 return 0;
             })
             this.setState({ services: servicesArr })
-            }).catch(error => {
-            console.log(error);
-            });
+            }).catch(error => { console.log(error) });
         })
+    }
+
+    fetchServiceById (Id) {
+        return this.props.contentful.getEntries({
+            content_type: 'service',
+            locale: this.props.locale,
+            'sys.id': Id
+        }).then(entry => { 
+            this.setState({ hmiService: entry.items[0].fields }) 
+        }).catch(err => console.log(err))
+
     }
 
     toggleActiveClass(index) {
         this.setState({ activeIndex: index });
     }
 
-    render () {
-        const renderSelection = FINs.map((item, index) => {
-            return (
-                <Link to={'/services/' + item + '/' + this.props.locale} key={index} onClick={this.toggleActiveClass.bind(this, index)}>
-                <div className={this.state.activeIndex === index ? 'select-item active' : 'select-item'}>
-                    <i className="fas fa-car-side fa-2x"></i>
-                    <p>{item}</p>
-                </div>
-            </Link>
-            )
-        })
-
-        if (this.state.services !== "") {
-            const renderServices = this.state.services.map((item, index) => {
-                return (
-                    <Service service={item.fields} key={index} renderModal={this.renderModal} />
-                )
-            })
-            return (
+    renderHMI() {
+        const { hmiService } = this.state
+        return (
             <div id="services">
-                <div id={this.state.modus === "car"  ? "hmi" : null }>
-                    {this.state.showModal ? this.renderModal : null}
-                    
-                    {this.state.modus === "desktop" ? 
-                        <div className="header">
-                            <h1>Fahrzeugaktivierung</h1>
-                        </div> 
-                    :         
-                    <h1>"HMI Fahrzeugaktivierung"</h1>
-                    }
-                    <div className={ this.state.modus === "car" ? "hmi-wrapper" : null}>
-                        <div className={this.state.modus === "car"  ? "article" : "content-wrapper"}>
-                            <div className="breadcrumb">
-                                <ol>
-                                    <li>Benutzerkonto erstellen</li>
-                                    <li>Fahrzeug hinzufügen</li>
-                                    <li className="active">Car-net bestellen</li>
-                                    <li>Car-net aktivieren</li>
-                                </ol>
+                <div id="hmi">
+                    <div className="hmi-wrapper">
+                        <article>
+                            <div className="text-icon-wrapper">
+                                <img className="icon hmi--icon" src={hmiService.icon.fields.file.url}  alt={hmiService.icon.fields.title} />
+                                <span className="title">{hmiService.name}</span>
                             </div>
-                            <h2>Bestellen Sie jetzt die folgenden mobilen Online-Dienste</h2>
-                            <br />
-                            <h3>Wählen Sie ihr Fahrzeug:</h3>
-                            <div className="selection">
-                                {renderSelection}
+                            <p className="description">{hmiService.description}</p>
+                            <div className="hmi--footer">
+                                <hr className="divider"/>
+                                <p className="text--back">Zurück</p>
                             </div>
-                            <br />
-                            <br />
-                            {renderServices}
-
-                            <p className="text-grey">
-                                Hinweis: Je nach Ausstattung Ihres Fahrzeugs und Land, in dem Sie sich befinden, stehen ggf. nicht alle (Teil-) Funktionen zur Verfügung.
-                            </p>
-                        </div>
-
-                        <section className={this.state.modus === "car"  ? "article" : "agb-submit content-wrapper"}>
-                            <h3>Widerrufsrecht</h3>
-                            <p>
-                                Sie haben das Recht, binnen vierzehn Tagen ohne Angabe von Gründen die Bestellung zu widerrufen. <br/>
-                                Die Einzelheiten entnhemen Sie bitte der <button className="btn--link">Widerrufsbelehrung</button> im Anhang zu den allgemeinen Geschäftsbedingungen.
-                            </p>
-                            <br />
-                            <h3>Allgemeine Geschäftsbedingungen</h3>
-                            <p>
-                                Die Allgemeinen Geschäftsbedingungen gelten für Sie in der bei Vertragsabschluss gültigen Fassung.
-                                Auf den Webseiten der Volkswagen AG finden sich die Allgemeinen Geschäftsbedingungen immer nur in der aktuellsten Version.
-                                Wir empfehlen Ihnen daher, die für Sie geltende Fassung dauerhaft aufzubewahren, indem Sie die Bestellbestätigung und/ oder das <button className="btn--link">hier</button> hinterlegte Dokument speichern.
-                            </p>
-                            <p><input type="checkbox" id="agb-submit"/><span className="checkmark"></span>Ich habe die <button className="btn--link">Allgemeinen Geschäftsbedingungen</button> gelesen und stimme ihrer Geltung hiermit zu.</p>
-                            <p className="text-grey">
-                                Hinweis: Mit dem Ender der Dienstelaufzeit endet der Vertrag über die mobilen Online-Dienste automatisch, ohne dass es einer Kündigung durch die Volkswagen AG oder den Kunden bedarf. 
-                                Die Volkswagen AG wird den Kunden rechtzeitig vor dem Ende der Dienstlaufzeit per E-Mail an eine Verlängerung erinnern. Hierzu kann der Kunde auf dem Car-Net Portal eine (Neu-) Bestellung
-                                der mobilen Online-Dienst zu den dann geltenden Laufzeiten und Konditionen vornehmen.
-                            </p>
-                            <button className="btn--primary">Car-Net Vertrag <br/> abschließen</button>
-                        </section>
+                        </article>   
                     </div>
                 </div>
                 <Footer
@@ -152,9 +105,96 @@ class Services extends Component {
                     icon={this.state.modus  === "desktop" ? "car" : "desktop"}
                 />
             </div>
+        )
+    }
+
+    renderDesktop(renderSelection, renderServices) {
+        return (
+            <div id="services">
+                {this.state.showModal ? this.renderModal : null}
+                <div className="header">
+                    <h1>Fahrzeugaktivierung</h1>
+                </div> 
+                <div className="content-wrapper">
+                    <div className="breadcrumb">
+                        <ol>
+                            <li>Benutzerkonto erstellen</li>
+                            <li>Fahrzeug hinzufügen</li>
+                            <li className="active">Car-net bestellen</li>
+                            <li>Car-net aktivieren</li>
+                        </ol>
+                    </div>
+                    <h2>Bestellen Sie jetzt die folgenden mobilen Online-Dienste</h2>
+                    <br />
+                    <h3>Wählen Sie ihr Fahrzeug:</h3>
+                    <div className="selection">
+                        {renderSelection}
+                    </div>
+                    <br />
+                    <br />
+                    {renderServices}
+
+                    <p className="text-grey">
+                        Hinweis: Je nach Ausstattung Ihres Fahrzeugs und Land, in dem Sie sich befinden, stehen ggf. nicht alle (Teil-) Funktionen zur Verfügung.
+                    </p>
+                </div>
+
+                <section className={this.state.modus === "car"  ? "article" : "agb-submit content-wrapper"}>
+                    <h3>Widerrufsrecht</h3>
+                    <p>
+                        Sie haben das Recht, binnen vierzehn Tagen ohne Angabe von Gründen die Bestellung zu widerrufen. <br/>
+                        Die Einzelheiten entnhemen Sie bitte der <button className="btn--link">Widerrufsbelehrung</button> im Anhang zu den allgemeinen Geschäftsbedingungen.
+                    </p>
+                    <br />
+                    <h3>Allgemeine Geschäftsbedingungen</h3>
+                    <p>
+                        Die Allgemeinen Geschäftsbedingungen gelten für Sie in der bei Vertragsabschluss gültigen Fassung.
+                        Auf den Webseiten der Volkswagen AG finden sich die Allgemeinen Geschäftsbedingungen immer nur in der aktuellsten Version.
+                        Wir empfehlen Ihnen daher, die für Sie geltende Fassung dauerhaft aufzubewahren, indem Sie die Bestellbestätigung und/ oder das <button className="btn--link">hier</button> hinterlegte Dokument speichern.
+                    </p>
+                    <p><input type="checkbox" id="agb-submit"/><span className="checkmark"></span>Ich habe die <button className="btn--link">Allgemeinen Geschäftsbedingungen</button> gelesen und stimme ihrer Geltung hiermit zu.</p>
+                    <p className="text-grey">
+                        Hinweis: Mit dem Ender der Dienstelaufzeit endet der Vertrag über die mobilen Online-Dienste automatisch, ohne dass es einer Kündigung durch die Volkswagen AG oder den Kunden bedarf. 
+                        Die Volkswagen AG wird den Kunden rechtzeitig vor dem Ende der Dienstlaufzeit per E-Mail an eine Verlängerung erinnern. Hierzu kann der Kunde auf dem Car-Net Portal eine (Neu-) Bestellung
+                        der mobilen Online-Dienst zu den dann geltenden Laufzeiten und Konditionen vornehmen.
+                    </p>
+                    <button className="btn--primary">Car-Net Vertrag <br/> abschließen</button>
+                </section>
+                <Footer
+                    toggleModus={this.toggleModus.bind(this)}
+                    icon={this.state.modus  === "desktop" ? "car" : "desktop"}
+                />
+            </div>
+        )
+    }
+
+    render () {
+        const renderSelection = FINs.map((item, index) => {
+            return (
+                <Link to={'/services/' + item + '/' + this.props.locale} key={index} onClick={this.toggleActiveClass.bind(this, index)}>
+                    <div className={this.state.activeIndex === index ? 'select-item active' : 'select-item'}>
+                        <i className="fas fa-car-side fa-2x"></i>
+                        <p>{item}</p>
+                    </div>
+                 </Link>
             )
+        })
+
+        if(this.state.modus === "car") {
+            if (this.state.hmiService !== "") {
+                return this.renderHMI()
+            }
         } else {
-            return <Spinner />;
+            if (this.state.services !== "") {
+                const renderServices = this.state.services.map((item, index) => {
+                    return (
+                        <Service service={item.fields} key={index} renderModal={this.renderModal} />
+                    )
+                })
+                return this.renderDesktop(renderSelection, renderServices)
+            } else {
+                return <Spinner />;
+            }
         }
     }
 }
